d24416df22478e7954e75b49cab7ca825e9c8834
turonAK <turonvalijonov397@gmail.com>
2025-07-17 18:32:12 +0500

    Update main.py

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ def calculate_distance(lat1, lon1, lat2, lon2):
     R = 6371000  # Земной радиус в метрах
     dlat = radians(lat2 - lat1)
     dlon = radians(lon2 - lon1)
-    a = sin(dlat/2)2 + cos(radians(lat1)) * cos(radians(lat2)) * sin(dlon/2)2
+    a = sin(dlat/2)**2 + cos(radians(lat1)) * cos(radians(lat2)) * sin(dlon/2)**2
     c = 2 * asin(sqrt(a))
     return R * c
 
